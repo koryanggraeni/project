aa26cdc90ce46eeef697c455e41de5b821c8d404
koryanggraeni <kory.anggraeni@gmail.com>
2017-04-10 16:41:56 +0700

    HelloWorld1

---
+++ HelloWorld1.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld1;
+using HelloWorld1.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void HomeIndexText()
+        {
+            //Arrange
+            HelloWorldController controller = new HelloWorldController();
+
+            //Act 
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Hello World!", );
+        }
+    }
+}
---
+++ HelloWorldTest-NUnit/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HelloWorldTest_NUnit
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+
+        public void HelloWorldTest()
+        {
+            Assert.True(1+1==3);
+        }
+    }
+}

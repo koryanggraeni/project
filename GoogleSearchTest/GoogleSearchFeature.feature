d77c4177b59b7e05b007914262a1ad2557b5d378
koryanggraeni <kory.anggraeni@gmail.com>
2017-04-10 14:05:26 +0700

    GoogleSearchTest

---
+++ GoogleSearchTest/GoogleSearchFeature.feature
@@ -0,0 +1,11 @@
+﻿Feature: GoogleSearch
+
+@mytag
+Scenario: Verify the search Functionality of Google Search page
+	Given I navigate to the page "www.google.com"
+	And I see the page is loaded
+	When I enter Search Keyword in the Search Text box
+	| Keyword  |
+	| SpecFlow |
+	And I click on Search Button
+	Then Search items shows the items related to SpecFlow
---
+++ GoogleSearchTest/GoogleSearchFeatureSteps.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace GoogleSearchTest
+{
+    [Binding]
+    public sealed class GoogleSearchFeatureSteps
+    {
+        IWebDriver driver;
+
+        [Given(@"I navigate to the page ""(.*)""")]
+        public void GivenINavigateToThePage(string p0)
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.google.com.bd");
+        }
+
+        [Given(@"I see the page is loaded")]
+        public void GivenISeeThePageIsLoaded()
+        {
+            Assert.AreEqual("Google",driver.Title);
+        }
+
+        [When(@"I enter Search Keyword in the Search Text box")]
+        public void WhenIEnterSearchKeywordInTheSearchTextBox(Table table)
+        {
+            string searchText = table.Rows[0]["Keyword"].ToString();
+            driver.FindElement(By.Name("q")).SendKeys(searchText);
+        }
+
+        [When(@"I click on Search Button")]
+        public void WhenIClickOnSearchButton()
+        {
+            driver.FindElement(By.Name("btnG")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+
+        [Then(@"Search items shows the items related to SpecFlow")]
+        public void ThenSearchItemsShowsTheItemsRelatedToSpecFlow()
+        {
+            Assert.AreEqual("SpecFlow - Cucumber for .NET", driver.FindElement(By.XPath("//h3/a")).Text);
+            driver.Close();
+        }
+
+    }
+}

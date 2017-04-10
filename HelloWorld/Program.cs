a3c699ab030a989ad61f800cdee03e3745377fb0
koryanggraeni <kory.anggraeni@gmail.com>
2017-04-10 14:31:19 +0700

    HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

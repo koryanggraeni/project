65e132e35731d8f5aae1c3b6b51478ded3e36176
koryanggraeni <kory.anggraeni@gmail.com>
2017-04-10 14:24:37 +0700

    Hello World

---
+++ Hello World/DemoVariable.cs
@@ -0,0 +1,70 @@
+﻿using HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class DemoVariable
+    {
+        public void DemoVariableMain()
+        {
+            HitungLuas();
+        }
+
+        private static void HitungLuas()
+        {
+            //deklarasi variable
+            int panjang, lebar, luas;
+            int[] param = InputParameter();
+
+            panjang = param[0];
+            lebar = param[1];
+
+            hitungLuasPersergi(panjang, lebar, out luas);
+            showResult(luas);
+        }
+
+        static int[] InputParameter()
+        {
+            Console.Write("Masukkan panjang: ");
+            string p = Console.ReadLine();
+            Console.Write("Masukkan lebar: ");
+            string l = Console.ReadLine();
+
+            int panjang = Int32.Parse(p);
+            int lebar = Int32.Parse(l);
+
+            int[] persegi = new int[5];
+            persegi[0] = panjang;
+            persegi[1] = lebar;
+
+            return persegi;
+        }
+
+        private static void hitungLuasPersergi(int panjang, int lebar, out int result)
+        {
+            result = panjang * lebar;
+
+            return;
+        }
+
+        private static void showResult(int result)
+        {
+            Console.WriteLine("Luas persegi panjang = " + result);
+            Console.WriteLine("if you want to calulate again input 1 or 2 for back to main menu: ");
+            string menu = Console.ReadLine();
+            if (menu == "1")
+            {
+                HitungLuas();
+            }
+            else
+            {
+                var instance = new Hello();
+                instance.chooseMenu();
+            }
+        }
+    }
+}
---
+++ Hello World/Program.cs
@@ -0,0 +1,65 @@
+﻿using Hello_World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            chooseMenu();
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        public static void chooseMenu()
+        {
+            Console.WriteLine("input 1 for demo exit or 2 for calculate: ");
+            string menu = Console.ReadLine();
+            if(menu == "1")
+            {
+                DemoExit();
+            }
+            else
+            {
+                var instance = new DemoVariable();
+                instance.DemoVariableMain();
+            }
+        }
+
+        private static void DemoExit()
+        {
+            int n;
+            Console.Write("Masukkan nilai n: ");
+            string s = Console.ReadLine();
+            
+            // Blok kode untuk proses pemilihan statemen
+            if (!int.TryParse(s, out n))
+            {
+                Console.WriteLine("ERROR: n harus berupa " +
+                                  "bilangan bulat");
+                DemoExit();
+            } // Akhir blok pemilihan
+
+            // Blok kode untuk proses pemilihan statemen
+            if (n < 0)
+            {
+                Console.WriteLine("ERROR: n tidak boleh negatif");
+                DemoExit();
+            } // Akhir blok pemilihan
+
+            // Blok kode untuk proses pengulangan statemen
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i);
+            } // Akhir blok pengulangan
+        }
+    }
+    
+}

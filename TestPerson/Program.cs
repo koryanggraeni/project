bfa2571bdc19b5e07c77207af94a5eec6b47c0ef
koryanggraeni <kory.anggraeni@gmail.com>
2017-04-10 18:04:50 +0700

    TestPerson

---
+++ TestPerson/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPerson
+{
+    public struct Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+    }
+
+    public class Lalala
+    {
+        public Lalala(int length)
+        {
+            var phrase = "lalalalalalalalalalalalalalalalalalalalalalalalalalalalalala";
+            var manyPhrases = new StringBuilder();
+            for (var i = 0; i < length; i++)
+            {
+                manyPhrases.Append(phrase);
+            }
+            Console.WriteLine("tra" + manyPhrases);
+
+            var syllable = "ha";
+            var laugh = "";
+            for (var i = 0; i < 10; i++)
+            {
+                laugh += syllable;
+                foreach (var ch in laugh)
+                {
+                    if (ch == 'h')
+                        Console.Write("H");
+                    else
+                        Console.Write(ch);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+
+    class ProgramPerson
+    {
+        static void Main()
+        {
+            //no parameter
+            Person p1 = new Person("Lee Min Ho", 28);
+            Console.WriteLine("person1 Name = {0} Age = {1}", p1.Name, p1.Age);
+
+            //using parameter
+            Person p2 = p1;
+
+            p2.Name = "Kim Woo Bin";
+            p2.Age = 27;
+
+            Console.WriteLine("person2 Name = {0} Age = {1}", p2.Name, p2.Age);
+            Console.WriteLine("person1 Name = {0} Age = {1}", p1.Name, p1.Age);
+
+            if (p2.Equals(p1))
+            {
+                Console.WriteLine("person2 and person1 have same values.");
+            }
+
+            Lalala l = new Lalala(10);
+
+
+            //keep the console window open in debug mode
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestPerson/WorkItem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPerson
+{
+    public class WorkItem
+    {
+        private static int currentID;
+
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        protected string Description { get; set; }
+        protected TimeSpan JobLength { get; set; }
+
+        public WorkItem()
+        {
+            ID = 0;
+            Title = "Default title";
+            Description = "Default description";
+            JobLength = new TimeSpan();
+        }
+
+        public WorkItem(string title, string desc, TimeSpan jobleng)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.Description = desc;
+            this.JobLength = jobleng;
+        }
+
+        static WorkItem()
+        {
+            currentID = 0;
+        }
+
+        protected int GetNextID()
+        {
+            return ++currentID;
+        }
+
+        public void Update(string title, TimeSpan jobleng)
+        {
+            this.Title = title;
+            this.JobLength = jobleng;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", this.ID, this.Title);
+        }
+    }
+
+    public class ChangeRequest : WorkItem
+    {
+        protected int originalItemID { get; set; }
+
+        public ChangeRequest()
+        {
+
+        }
+
+        public ChangeRequest(string title, string desc, TimeSpan jobleng, int originalID)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.Description = desc;
+            this.JobLength = jobleng;
+
+            this.originalItemID = originalID;
+        }
+    }
+
+    class ProgramWorkItem
+    {
+        static void Main()
+        {
+            WorkItem item = new WorkItem("Create bug ticket", "create all bugs ticket", new TimeSpan(1, 2, 3));
+            ChangeRequest change = new ChangeRequest("change bug ticket", "change bug ticket description", new TimeSpan(4, 3, 2), 1);
+            Console.WriteLine(item.ToString());
+
+            change.Update("Change bugs ticket description for some cases", new TimeSpan(3, 2, 1));
+            Console.WriteLine(change.ToString());
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}

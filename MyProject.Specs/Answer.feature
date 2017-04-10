c7d7448e42b45a2f029b95b1cfa7565b01370378
koryanggraeni <kory.anggraeni@gmail.com>
2017-04-10 17:57:42 +0700

    MyProject.Specs

---
+++ MyProject.Specs/Answer.feature
@@ -0,0 +1,10 @@
+﻿Feature: Ordering answers
+
+@mytag
+Scenario: the answer with highest vote get to the top
+	Given there is a question "What's your favorite colour?" with the answers
+	| Answer         | Vote |
+	| red            | 1    |
+	| Cucumber green | 1    |
+	When you upvote answer "Cucumber green"
+	Then the answer "Cucumber green" should be on top
---
+++ MyProject.Specs/OrderingAnswersSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyProject.Specs
+{
+    [Binding]
+    public class OrderingAnswersSteps
+    {
+        IWebDriver driver;
+        [Given(@"there is a question ""(.*)"" with the answers")]
+        public void GivenThereIsAQuestionWithTheAnswers(string p0, Table table)
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.google.co.id");
+        }
+        
+        [When(@"you upvote answer ""(.*)""")]
+        public void WhenYouUpvoteAnswer(string p0)
+        {
+            
+        }
+        
+        [Then(@"the answer ""(.*)"" should be on top")]
+        public void ThenTheAnswerShouldBeOnTop(string p0)
+        {
+            
+        }
+    }
+}
